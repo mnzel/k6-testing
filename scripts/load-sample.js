cdc61ff5cfd8fd77ed675be60ff3b7a777fcbbb8
Manzeel <manzeel.uprety@bitsbeat.com>
2022-03-06 20:25:51 +0545

    new edit

--- scripts/load-sample.js
+++ scripts/load-sample.js
@@ -14,9 +14,9 @@ export let options = {
     insecureSkipTLSVerify: true,
     noConnectionReuse: false,
     stages: [
-        { duration: '5m', target: 100 },
-        { duration: '10m', target: 100 },
-        { duration: '4m', target: 0 }, 
+        { duration: '2m', target: 100 },
+        { duration: '1m', target: 150 },
+        { duration: '1m', target: 0 }, 
         
     ],
 
